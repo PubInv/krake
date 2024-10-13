f3247d5e2b4ca1ee1d7fc22c649e3068d8ae9a9a
Forrest Erickson <forresterickson@comcast.net>
2024-10-13 18:26:07 -0400

    Move Wink into a tab on the PMD_Nothing sketch.

--- PMD/PMD_Nothing/PMD_Nothing/PMD_Nothing.ino
+++ PMD/PMD_Nothing/PMD_Nothing/PMD_Nothing.ino
@@ -13,48 +13,22 @@
 #define ORIGIN "USA"
 
 /* Add a detailed description here
- *  This device is the best PMD ever. It does nothing except wink the build in LED On pin 2).
+ *  This device is the best PMD ever. 
+ *  It does nothing except wink the build in LED On pin 2).
  */
 
+#define BAUDRATE 115200 //Serial port
 
 // Some PMD Hardware
 // Pins for switches and LEDs and more
 #define BOOT_BUTTON 0
 
-
-
-// Heart beat aka activity indicator LED.
-//Set LED for Uno or ESP32 Dev Kit on board blue LED.
-const int LED_BUILTIN = 2;    // ESP32 Kit
-//const int LED_BUILTIN = 13;    //Not really needed for Arduino UNO it is defined in library
-const int HIGH_TIME_LED = 900;
-const int LOW_TIME_LED = 100;
-unsigned long lastLEDtime = 0;
-unsigned long nextLEDchange = 100; //time in ms.
-
-
-#define BAUDRATE 115200
-
-
 //Some more functions
-
-//Wink the LED
-void wink(void) {
-  if (((millis() - lastLEDtime) > nextLEDchange) || (millis() < lastLEDtime)) {
-    if (digitalRead(LED_BUILTIN) == LOW) {
-      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-      nextLEDchange = HIGH_TIME_LED;
-    } else {
-      digitalWrite(LED_BUILTIN, LOW);   // turn the LED on (HIGH is the voltage level)
-      nextLEDchange = LOW_TIME_LED;
-    }
-    lastLEDtime = millis();
-  }
-}//end LED wink
 
 
 // The Setup
 void setup() {
+  const int LED_BUILTIN = 2;    // ESP32 Kit
   pinMode(LED_BUILTIN, OUTPUT);      // set the LED pin mode
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
 
@@ -64,21 +38,20 @@ void setup() {
   }
   delay(500);
   //Serial splash
-  Serial.println("===================================");
+  Serial.println(F("==================================="));
   Serial.print(PROG_NAME);
   Serial.println(VERSION);
   Serial.println(MODEL_NAME);
   Serial.println(DEVICE_UNDER_TEST);
-  Serial.print("Compiled at: ");
+  Serial.print(F("Compiled at: "));
   Serial.println(F(__DATE__ " " __TIME__) ); //compile date that is used for a unique identifier
   Serial.println(LICENSE);
-  Serial.println("===================================");
+  Serial.println(F("==================================="));
   Serial.println();
 
 // More setup code here
 
-  digitalWrite(LED_BUILTIN, LOW);
-
+  digitalWrite(LED_BUILTIN, LOW);  //Make built in LED low at end of setup.
 }//end setup()
 
 void loop() {
