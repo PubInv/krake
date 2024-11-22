400eee3be86c206ee4f7e60fd7a0a5ab410d8eea
Nagham <Naghamkheir25@gmail.com>
2024-11-22 12:59:39 -0500

    Updating ESP32 schematic symbol and added the df player into the audio schematic.

--- Firmware/GPAD_API/gpad_utility.h
+++ Firmware/GPAD_API/gpad_utility.h
@@ -25,7 +25,7 @@
 
 
 #define PROG_NAME "GPAD_API "
-#define FIRMWARE_VERSION "0.19 "
+#define FIRMWARE_VERSION "0.19_20241119"
 //#define HARDWARE_VERSION "V0.0.1 "
 #define MODEL_NAME "KRAKE_"
 //#define DEVICE_UNDER_TEST "20240421_LEB1"  //A Serial Number
