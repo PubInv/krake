67c457402e1e7c5f94484b0fc540e163344af30b
Nagham <Naghamkheir25@gmail.com>
2025-02-16 09:41:17 -0500

    Used the Flasher class to manage the 5 alarm LEDs on HW2 board, PMD, with independant periods and independent duty cycles.

--- PMD/PMD_Multitasking/PMD_Multitasking.ino
+++ PMD/PMD_Multitasking/PMD_Multitasking.ino
@@ -7,7 +7,7 @@
 
 // Customized this by changing these defines
 
-#define VERSION "V0.0.1 "
+#define VERSION "V0.0.2 "
 #define MODEL_NAME "Model: HW2_Multitasking"
 #define DEVICE_UNDER_TEST "SN: 00001"  //A Serial Number
 #define LICENSE "GNU Affero General Public License, version 3 "
@@ -26,12 +26,55 @@ const int LED_3 = 5;
 const int LED_4 = 18;
 const int LED_5 = 19;
 
+
 /* Add a detailed description here
  *  This device is the best PMD ever. 
  *  It does nothing except wink LEDs).
  */
 
-//Some more functions
+//Claases defined below
+class Flasher {
+  // Class Member Variables
+  // These are initialized at startup
+  int ledPin;    // the number of the LED pin
+  long OnTime;   // milliseconds of on-time
+  long OffTime;  // milliseconds of off-time
+
+  // These maintain the current state
+  int ledState;                  // ledState used to set the LED
+  unsigned long previousMillis;  // will store last time LED was updated
+
+  // Constructor - creates a Flasher
+  // and initializes the member variables and state
+public:
+  Flasher(int pin, long on, long off) {
+    ledPin = pin;
+    pinMode(ledPin, OUTPUT);
+
+    OnTime = on;
+    OffTime = off;
+
+    ledState = LOW;
+    previousMillis = 0;
+  }
+
+  void Update() {
+    // check to see if it's time to change the state of the LED
+    unsigned long currentMillis = millis();
+
+    if ((ledState == HIGH) && (currentMillis - previousMillis >= OnTime)) {
+      ledState = LOW;                  // Turn it off
+      previousMillis = currentMillis;  // Remember the time
+      digitalWrite(ledPin, ledState);  // Update the actual LED
+    } else if ((ledState == LOW) && (currentMillis - previousMillis >= OffTime)) {
+      ledState = HIGH;                 // turn it on
+      previousMillis = currentMillis;  // Remember the time
+      digitalWrite(ledPin, ledState);  // Update the actual LED
+    }
+  }
+};
+
+//functions defined below
 
 void serialSplash() {
   //Serial splash
@@ -46,6 +89,22 @@ void serialSplash() {
   Serial.println(F("==================================="));
   Serial.println();
 }
+//Defining the behavior
+/*
+LED 1 blinks 0.5 period and 50% duty cycle
+LED 2 blinks 1.2 period and 20% duty cycle
+LED 3 blinks 1.4 period and 62% duty cycle
+LED 4 blinks 3.0 period and 55% duty cycle
+LED 5 blinks 7.5 period and 10% duty cycle
+*/
+
+// Flasher led1(ledpin#, on time, off time);
+Flasher led1(LED_1, 250, 250);
+Flasher led2(LED_2, 1200 * 20 / 100, 1200 * 80 / 100);
+Flasher led3(LED_3, 1400 * 62 / 100, 1400 * 38 / 100);
+Flasher led4(LED_4, 3000 * 55 / 100, 3000 * 45 / 100);
+Flasher led5(LED_5, 7500 * 10 / 100, 7500 * 90 / 100);
+
 
 // The Setup
 void setup() {
@@ -53,16 +112,16 @@ void setup() {
   //setting GPIO to initial values
   pinMode(LED_BUILTIN, OUTPUT);     // set the LED pin mode
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  pinMode(LED_1, OUTPUT);           // set the LED pin mode
-  digitalWrite(LED_1, HIGH);        // turn the LED on (HIGH is the voltage level)
-  pinMode(LED_2, OUTPUT);           // set the LED pin mode
-  digitalWrite(LED_2, HIGH);        // turn the LED on (HIGH is the voltage level)
-  pinMode(LED_3, OUTPUT);           // set the LED pin mode
-  digitalWrite(LED_3, HIGH);        // turn the LED on (HIGH is the voltage level)
-  pinMode(LED_4, OUTPUT);           // set the LED pin mode
-  digitalWrite(LED_4, HIGH);        // turn the LED on (HIGH is the voltage level)
-  pinMode(LED_5, OUTPUT);           // set the LED pin mode
-  digitalWrite(LED_5, HIGH);        // turn the LED on (HIGH is the voltage level)
+  // pinMode(LED_1, OUTPUT);           // set the LED pin mode
+  // digitalWrite(LED_1, HIGH);        // turn the LED on (HIGH is the voltage level)
+  // pinMode(LED_2, OUTPUT);           // set the LED pin mode
+  // digitalWrite(LED_2, HIGH);        // turn the LED on (HIGH is the voltage level)
+  // pinMode(LED_3, OUTPUT);           // set the LED pin mode
+  // digitalWrite(LED_3, HIGH);        // turn the LED on (HIGH is the voltage level)
+  // pinMode(LED_4, OUTPUT);           // set the LED pin mode
+  // digitalWrite(LED_4, HIGH);        // turn the LED on (HIGH is the voltage level)
+  // pinMode(LED_5, OUTPUT);           // set the LED pin mode
+  // digitalWrite(LED_5, HIGH);        // turn the LED on (HIGH is the voltage level)
 
   Serial.begin(BAUDRATE);
   while (!Serial) {
@@ -73,16 +132,22 @@ void setup() {
   serialSplash();
 
   // More setup code here
-  digitalWrite(LED_1, LOW);        //Make built in LED low at end of setup.
-  digitalWrite(LED_2, LOW);        //Make built in LED low at end of setup.
-  digitalWrite(LED_3, LOW);        //Make built in LED low at end of setup.
-  digitalWrite(LED_4, LOW);        //Make built in LED low at end of setup.
-  digitalWrite(LED_5, LOW);        //Make built in LED low at end of setup.
+  // digitalWrite(LED_1, LOW);        //Make built in LED low at end of setup.
+  // digitalWrite(LED_2, LOW);        //Make built in LED low at end of setup.
+  // digitalWrite(LED_3, LOW);        //Make built in LED low at end of setup.
+  // digitalWrite(LED_4, LOW);        //Make built in LED low at end of setup.
+  // digitalWrite(LED_5, LOW);        //Make built in LED low at end of setup.
   digitalWrite(LED_BUILTIN, LOW);  //Make built in LED low at end of setup.
 }  //end setup()
 
 void loop() {
   // More loop code here
-  digitalWrite(LED_1, HIGH);
+  // digitalWrite(LED_1, HIGH);
   wink();
+  led1.Update();
+  led2.Update();
+  led3.Update();
+  led4.Update();
+  led5.Update();
+
 }  //end loop()
