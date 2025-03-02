5ff974cdc68b2f8cb06a64df5debdb0a0f03433c
Nagham <Naghamkheir25@gmail.com>
2025-03-02 08:45:42 -0500

    Code cleanup.

--- PMD/PMD_BOOTUP/LCD.ino
+++ PMD/PMD_BOOTUP/LCD.ino
@@ -17,40 +17,27 @@ void LCD_setup() {
   // Print a message to the LCD.
   lcd.backlight();
   lcd.setCursor(0, 0);
-  // lcd.print("Hello, Public Invention!");
   // lcd.print("Hello, Public Invention!qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwer");
   // lcd.print("Hello, Public Invention!qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwer0123456789");
   // lcd.print("wwwwwwwww/xxxxxxxxx/yyyyyyyyy/zzzzzzzzz/wwwwwwwww/xxxxxxxxx/yyyyyyyyy/zzzzzzzzz/");
   // lcd.print("The quick brown fox jumps over the lazy dog near the river at sunset today!testlcd");
-  lcd.print("The quick brown fox jumps over the lazy dog near the river at sunset today!test0");
-  delay(1000);
-  lcd.print("123456789");
-
-
-  // lcd.setCursor(4, 2);
-  // lcd.print("I am Krake!");
+  // lcd.print("The quick brown fox jumps over the lazy dog near the river at sunset today!test0");
+  // delay(1000);
+  // lcd.print("123456789");
 }
 
-
 void LCD_Splash(void) {
-
   // lcd.print("wwwwwwwww/xxxxxxxxx/yyyyyyyyy/zzzzzzzzz/wwwwwwwww/xxxxxxxxx/yyyyyyyyy/zzzzzzzzz/");
-  //  lcd.print(F("===================="));
   lcd.clear();
   lcd.setCursor(0, 0);  // home cursor
   lcd.print(PROG_NAME);
   lcd.print(VERSION);
-  delay(5000);
+  // delay(5000);
   lcd.setCursor(0, 2);  //set cursor (culomn,raw)
-
   // lcd.print(MODEL_NAME);
   // lcd.print(DEVICE_UNDER_TEST);
-  lcd.setCursor(0, 3);  //set cursor (culomn,raw)
   lcd.print(F("Compiled at: "));
-  delay(5000);
   lcd.setCursor(0, 3);  //set cursor (culomn,raw)
   lcd.print(F(__DATE__ "" __TIME__));  //compile date that is used for a unique identifier
   // lcd.print(LICENSE);
-  // lcd.print(F("===================="));
-  // lcd.print();
 }
