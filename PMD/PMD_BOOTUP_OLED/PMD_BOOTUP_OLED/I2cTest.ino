b76260c5e3e663d18e5992cd0d661b3e16499f7e
Nagham <Naghamkheir25@gmail.com>
2025-03-14 14:55:13 -0400

    Managed the OLED line spacing.

--- PMD/PMD_BOOTUP_OLED/PMD_BOOTUP_OLED/I2cTest.ino
+++ PMD/PMD_BOOTUP_OLED/PMD_BOOTUP_OLED/I2cTest.ino
@@ -13,8 +13,12 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void test() {
-  Serial.begin(115200);
 
+  int16_t rowPosition = 0;
+  int16_t columnPosition = 0;
+  int16_t rowHeight = 10;  //just a guess
+
+  // Serial.begin(115200);
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {  // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     //for(;;); // tODO: DO NOT TRAP AN ERROR.SET A FLAG.
@@ -24,16 +28,20 @@ void test() {
   display.setTextSize(1);
   display.setTextColor(WHITE);
   // display.setCursor( SCREEN_WIDTH/3,  SCREEN_HEIGHT/3);
-  display.setCursor(0, 0);
-  // Display static text
+  // OLED Splash Screen
+  display.setCursor(0, rowPosition);
   display.println("Hello, I am a PMD!");
-  display.setCursor(0, SCREEN_HEIGHT / 5);
+  rowPosition = rowPosition + rowHeight;
+  display.setCursor(0, rowPosition);
   display.print(PROG_NAME);
-  display.setCursor(0, SCREEN_HEIGHT / 3);
+  rowPosition = rowPosition + rowHeight;
+  display.setCursor(0, rowPosition);
   display.print(VERSION);
-  display.setCursor(0, SCREEN_HEIGHT / 2);
-  display.print(F("Compiled at:" ));
-  display.setCursor(0, SCREEN_HEIGHT / 1.5);
+  rowPosition = rowPosition + rowHeight;
+  display.setCursor(0, rowPosition);
+  display.print(F("Compiled at:"));
+  rowPosition = rowPosition + rowHeight;
+  display.setCursor(0, rowPosition);
   display.print(F(__DATE__ " " __TIME__));
 
   display.display();
