1bba8f91c8ba8f49906a8957ecc18d6a09fe00e5
Nagham <Naghamkheir25@gmail.com>
2025-03-30 08:46:08 -0400

    WiFiready Elegance code with buttons compiled.

--- PMD/PMD_WiFiReady_Elegant/PMD_WiFiReady_Elegant.ino
+++ PMD/PMD_WiFiReady_Elegant/PMD_WiFiReady_Elegant.ino
@@ -11,11 +11,12 @@
 #include <ESPAsyncWebServer.h>
 #include "LittleFS.h"
 #include <ElegantOTA.h>
-#include <FS.h>  // File System Support
-#include <Wire.h> // req for i2c comm
+#include <FS.h>    // File System Support
+#include <Wire.h>  // req for i2c comm
 #include <DFRobotDFPlayerMini.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <DailyStruggleButton.h>
 
 // Customized this by changing these defines
 #define VERSION " V0.0.1 "
@@ -50,17 +51,17 @@ const int LED_4 = 18;
 const int LED_5 = 19;
 const int WiFiLed = 23;  // Built-in LED on ESP32
 
-const int SW1 = 36;
-const int SW2 = 39;
-const int SW3 = 34;
-const int SW4 = 35;
+// const int SW1 = 36;
+// const int SW2 = 39;
+// const int SW3 = 34;
+// const int SW4 = 35;
 
 
 
 const int LED_PINS[] = { LED_1, LED_2, LED_3, LED_4, LED_5 };
-const int SWITCH_PINS[] = { SW1, SW2, SW3, SW4 };  // SW1, SW2, SW3, SW4
+// const int SWITCH_PINS[] = { SW1, SW2, SW3, SW4 };  // SW1, SW2, SW3, SW4
 const int LED_COUNT = sizeof(LED_PINS) / sizeof(LED_PINS[0]);
-const int SWITCH_COUNT = sizeof(SWITCH_PINS) / sizeof(SWITCH_PINS[0]);
+// const int SWITCH_COUNT = sizeof(SWITCH_PINS) / sizeof(SWITCH_PINS[0]);
 
 // bool dfpAvailable = false;
 bool availableDFPLAYER = false;
@@ -135,134 +136,143 @@ Flasher led5(LED_5, 7500 * 10 / 100, 7500 * 90 / 100);  // LED 5 blinks 7.5 peri
 
 // The Setup
 
-void IRAM_ATTR switchISR() {
-  for (int i = 0; i < SWITCH_COUNT; i++) {
-    if (digitalRead(SWITCH_PINS[i]) == LOW) {  // Assuming active LOW
-      notifyClients("SW" + String(i + 1) + " pressed!");
-    }
-  }
-}
+// void IRAM_ATTR switchISR() {
+//   for (int i = 0; i < SWITCH_COUNT; i++) {
+//     if (digitalRead(SWITCH_PINS[i]) == LOW) {  // Assuming active LOW
+//       notifyClients("SW" + String(i + 1) + " pressed!");
+//     }
+//   }
+// }
 
 
-void setup() {
-  pinMode(SW1, INPUT);  // sets the digital pin 36 as input
-  pinMode(SW2, INPUT);  // sets the digital pin 39 as input
-  pinMode(SW3, INPUT);  // sets the digital pin 34 as input
-  pinMode(SW4, INPUT);  // sets the digital pin 35 as input
+// void setup() {
 
-  //setting GPIO to initial values
-  pinMode(WiFiLed, OUTPUT);
+//   // pinMode(SW1, INPUT);  // sets the digital pin 36 as input
+//   // pinMode(SW2, INPUT);  // sets the digital pin 39 as input
+//   // pinMode(SW3, INPUT);  // sets the digital pin 34 as input
+//   // pinMode(SW4, INPUT);  // sets the digital pin 35 as input
 
-  pinMode(LED_BUILTIN, OUTPUT);     // set the LED pin mode
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  pinMode(LED_1, OUTPUT);           // set the LED pin mode
-  digitalWrite(LED_1, HIGH);        // turn the LED on (HIGH is the voltage level)
-  pinMode(LED_2, OUTPUT);           // set the LED pin mode
-  digitalWrite(LED_2, HIGH);        // turn the LED on (HIGH is the voltage level)
-  pinMode(LED_3, OUTPUT);           // set the LED pin mode
-  digitalWrite(LED_3, HIGH);        // turn the LED on (HIGH is the voltage level)
-  pinMode(LED_4, OUTPUT);           // set the LED pin mode
-  digitalWrite(LED_4, HIGH);        // turn the LED on (HIGH is the voltage level)
-  pinMode(LED_5, OUTPUT);           // set the LED pin mode
-  digitalWrite(LED_5, HIGH);        // turn the LED on (HIGH is the voltage level)
+//   //setting GPIO to initial values
+//   pinMode(WiFiLed, OUTPUT);
 
-  Serial.begin(BAUDRATE);
-  while (!Serial) {
-    ;  // wait for serial port to connect. Needed for native USB
-  }
-  WiFiMan();
-  // blink();
-  splashserial();
-  Wire.begin();
-  initOLED();
-  splashOLED();
-  initDFP();
-  splashDFPlayer();
+//   pinMode(LED_BUILTIN, OUTPUT);     // set the LED pin mode
+//   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+//   pinMode(LED_1, OUTPUT);           // set the LED pin mode
+//   digitalWrite(LED_1, HIGH);        // turn the LED on (HIGH is the voltage level)
+//   pinMode(LED_2, OUTPUT);           // set the LED pin mode
+//   digitalWrite(LED_2, HIGH);        // turn the LED on (HIGH is the voltage level)
+//   pinMode(LED_3, OUTPUT);           // set the LED pin mode
+//   digitalWrite(LED_3, HIGH);        // turn the LED on (HIGH is the voltage level)
+//   pinMode(LED_4, OUTPUT);           // set the LED pin mode
+//   digitalWrite(LED_4, HIGH);        // turn the LED on (HIGH is the voltage level)
+//   pinMode(LED_5, OUTPUT);           // set the LED pin mode
+//   digitalWrite(LED_5, HIGH);        // turn the LED on (HIGH is the voltage level)
+
+//   Serial.begin(BAUDRATE);
+//   while (!Serial) {
+//     ;  // wait for serial port to connect. Needed for native USB
+//   }
+//   WiFiMan();
+//   // blink();
+//   splashserial();
+//   Wire.begin();
+//   initOLED();
+//   splashOLED();
+//   initDFP();
+//   splashDFPlayer();
 
 
-  // More setup code here
+//   // More setup code here
 
-  digitalWrite(LED_1, LOW);        //Make built in LED low at end of setup.
-  digitalWrite(LED_2, LOW);        //Make built in LED low at end of setup.
-  digitalWrite(LED_3, LOW);        //Make built in LED low at end of setup.
-  digitalWrite(LED_4, LOW);        //Make built in LED low at end of setup.
-  digitalWrite(LED_5, LOW);        //Make built in LED low at end of setup.
-  digitalWrite(LED_BUILTIN, LOW);  //Make built in LED low at end of setup.
+//   digitalWrite(LED_1, LOW);        //Make built in LED low at end of setup.
+//   digitalWrite(LED_2, LOW);        //Make built in LED low at end of setup.
+//   digitalWrite(LED_3, LOW);        //Make built in LED low at end of setup.
+//   digitalWrite(LED_4, LOW);        //Make built in LED low at end of setup.
+//   digitalWrite(LED_5, LOW);        //Make built in LED low at end of setup.
+//   digitalWrite(LED_BUILTIN, LOW);  //Make built in LED low at end of setup.
 
-  //Elegant OTA Setup
+//   //Elegant OTA Setup
 
-  initWiFi();
-  initLittleFS();
- 
+//   initWiFi();
+//   initLittleFS();
 
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(LittleFS, "/index.html", "text/html", false, processor);
-  });
 
-  server.serveStatic("/", LittleFS, "/");
+//   // Route for root / web page
+//   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+//     request->send(LittleFS, "/index.html", "text/html", false, processor);
+//   });
 
-  // Route to control LEDs using parameters
-  server.on("/control", HTTP_GET, [](AsyncWebServerRequest *request) {
-    if (request->hasParam("led") && request->hasParam("state")) {
-      int ledIndex = request->getParam("led")->value().toInt();
-      String state = request->getParam("state")->value();
+//   server.serveStatic("/", LittleFS, "/");
 
-      if (ledIndex >= 1 && ledIndex <= LED_COUNT) {
-        digitalWrite(LED_PINS[ledIndex - 1], (state == "on") ? HIGH : LOW);
-        request->send(200, "text/plain", "OK");
-      } else {
-        request->send(400, "text/plain", "Invalid LED index");
-      }
-    } else {
-      request->send(400, "text/plain", "Missing parameters");
-    }
-  });
+//   // Route to control LEDs using parameters
+//   server.on("/control", HTTP_GET, [](AsyncWebServerRequest *request) {
+//     if (request->hasParam("led") && request->hasParam("state")) {
+//       int ledIndex = request->getParam("led")->value().toInt();
+//       String state = request->getParam("state")->value();
 
-  // Initialize switch pins as inputs with pull-ups
-  for (int i = 0; i < SWITCH_COUNT; i++) {
-    pinMode(SWITCH_PINS[i], INPUT_PULLUP);
-    attachInterrupt(SWITCH_PINS[i], switchISR, FALLING);
-  }
+//       if (ledIndex >= 1 && ledIndex <= LED_COUNT) {
+//         digitalWrite(LED_PINS[ledIndex - 1], (state == "on") ? HIGH : LOW);
+//         request->send(200, "text/plain", "OK");
+//       } else {
+//         request->send(400, "text/plain", "Invalid LED index");
+//       }
+//     } else {
+//       request->send(400, "text/plain", "Missing parameters");
+//     }
+//   });
 
-  // Start server
-  server.begin();
-  ElegantOTA.begin(&server);  // Start ElegantOTA
+//   // // Initialize switch pins as inputs with pull-ups
+//   // for (int i = 0; i < SWITCH_COUNT; i++) {
+//   //   pinMode(SWITCH_PINS[i], INPUT_PULLUP);
+//   //   attachInterrupt(SWITCH_PINS[i], switchISR, FALLING);
+//   // }
 
-  // End of ELegant OTA Setup
-}
-//end setup()
+//   // Start server
+//   server.begin();
+//   ElegantOTA.begin(&server);  // Start ElegantOTA
 
-void loop() {
-  if (availableDFPLAYER) {
-    if (!digitalRead(SW1)) {
-      dfPlayer.play(1);
-      Serial.print(digitalRead(SW1));
-      Serial.println(", playing track 1");
-    }
-    if (!digitalRead(SW2)) {
-      dfPlayer.play(2);
-      Serial.print(digitalRead(SW2));
-      Serial.println(", playing track 2");
-    }
-    if (!digitalRead(SW3)) {
-      dfPlayer.play(3);
-      Serial.print(digitalRead(SW3));
-      Serial.println(", playing track 3");
-    }
-    if (!digitalRead(SW4)) {
-      dfPlayer.play(5);
-      Serial.print(digitalRead(SW4));
-      Serial.println(", playing track 5");
-    }
-  }
-  // displayAlarm(); // check OLED i2cTest code and fix// OLED is not displaying the alarm message when the SW of emergency is trigerred
+//   // End of ELegant OTA Setup
+//   Buttonsetup();
+// }
+// //end setup()
 
-  switchISR();
-  wink();  // Heart beat aka activity indicator LED function.
-  led1.Update();
-  led2.Update();
-  led3.Update();
-  led4.Update();
-  led5.Update();
-}  //end loop()
+// void loop() {
+//   // if (availableDFPLAYER) {
+//   //   if (!digitalRead(SW1)) {
+//   //     dfPlayer.play(1);
+//   //     Serial.print(digitalRead(SW1));
+//   //     Serial.println(", playing track 1");
+//   //   }
+//   //   if (!digitalRead(SW2)) {
+//   //     dfPlayer.play(2);
+//   //     Serial.print(digitalRead(SW2));
+//   //     Serial.println(", playing track 2");
+//   //   }
+//   //   if (!digitalRead(SW3)) {
+//   //     dfPlayer.play(3);
+//   //     Serial.print(digitalRead(SW3));
+//   //     Serial.println(", playing track 3");
+//   //   }
+//   //   if (!digitalRead(SW4)) {
+//   //     dfPlayer.play(5);
+//   //     Serial.print(digitalRead(SW4));
+//   //     Serial.println(", playing track 5");
+//   //   }
+//   // }
+//   // displayAlarm(); // check OLED i2cTest code and fix// OLED is not displaying the alarm message when the SW of emergency is trigerred
+
+//   // switchISR();
+//   wink();  // Heart beat aka activity indicator LED function.
+//   led1.Update();
+//   led2.Update();
+//   led3.Update();
+//   led4.Update();
+//   led5.Update();
+
+//   // This is needed to poll the button constantly
+//   myButton.poll();
+//   SW1.poll();
+//   SW2.poll();
+//   SW3.poll();
+//   SW4.poll();
+// }  //end loop()
