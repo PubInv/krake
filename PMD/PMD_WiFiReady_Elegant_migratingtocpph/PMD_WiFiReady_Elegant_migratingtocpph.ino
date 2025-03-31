84fa6e6c3970c3ecdec76a86dea870adf800c87a
Nagham <Naghamkheir25@gmail.com>
2025-03-31 04:40:40 -0400

    Code cleaned and functions enhanced.

--- PMD/PMD_WiFiReady_Elegant_migratingtocpph/PMD_WiFiReady_Elegant_migratingtocpph.ino
+++ PMD/PMD_WiFiReady_Elegant_migratingtocpph/PMD_WiFiReady_Elegant_migratingtocpph.ino
@@ -22,27 +22,14 @@
 #include "OLEDDisplay.h"
 #include "Wink.h"
 
-
-// // Customized this by changing these defines
-// #define VERSION " V0.0.1 "
-// #define MODEL_NAME "Model: HW2_WiFiReady_Elegant"
-// #define DEVICE_UNDER_TEST "SN: 00001"  //A Serial Number
-// #define LICENSE "GNU Affero General Public License, version 3 "
-// #define ORIGIN "LB"
-#define BAUDRATE 115200  //Serial port
-
-
-// HardwareSerial mySerial2(2);  // Use UART2
-// DFRobotDFPlayerMini dfPlayer;
-
-// Create AsyncWebServer object on port 80
+#define BAUDRATE 115200  //Serial port \
+                         // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 
 void notifyClients(const String &message) {
   ws.textAll(message);
 }
-
 
 // Some PMD Hardware
 
@@ -55,9 +42,6 @@ const int LED_3 = 5;
 const int LED_4 = 18;
 const int LED_5 = 19;
 int WiFiLed = 23;  // Built-in LED on ESP32
-
-
-
 
 const int LED_PINS[] = { LED_1, LED_2, LED_3, LED_4, LED_5 };
 // const int SWITCH_PINS[] = { SW1, SW2, SW3, SW4 };  // SW1, SW2, SW3, SW4
@@ -110,23 +94,6 @@ public:
   }
 };
 
-
-
-
-// void splashserial(void) {
-//   //Serial splash
-//   Serial.println(F("==================================="));
-//   Serial.print(PROG_NAME);
-//   Serial.println(VERSION);
-//   Serial.println(MODEL_NAME);
-//   Serial.println(DEVICE_UNDER_TEST);
-//   Serial.print(F("Compiled at: "));
-//   Serial.println(F(__DATE__ " " __TIME__));  //compile date that is used for a unique identifier
-//   Serial.println(LICENSE);
-//   Serial.println(F("==================================="));
-//   Serial.println();
-// }
-
 //Defining the behavior
 // Flasher led1(ledpin#, on time, off time);
 Flasher led1(LED_1, 250, 250);                          // LED 1 blinks 0.5 period and 50% duty cycle
@@ -134,3 +101,94 @@ Flasher led2(LED_2, 1200 * 20 / 100, 1200 * 80 / 100);  // LED 2 blinks 1.2 peri
 Flasher led3(LED_3, 1400 * 62 / 100, 1400 * 38 / 100);  // LED 3 blinks 1.4 period and 62% duty cycle
 Flasher led4(LED_4, 3000 * 55 / 100, 3000 * 45 / 100);  // LED 4 blinks 3.0 period and 55% duty cycle
 Flasher led5(LED_5, 7500 * 10 / 100, 7500 * 90 / 100);  // LED 5 blinks 7.5 period and 10% duty cycle
+
+
+//Elegant OTA Setup
+
+void setupOTA() {
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(LittleFS, "/index.html", "text/html", false, processor);
+  });
+
+  server.serveStatic("/", LittleFS, "/");
+
+  // Route to control LEDs using parameters
+  server.on("/control", HTTP_GET, [](AsyncWebServerRequest *request) {
+    if (request->hasParam("led") && request->hasParam("state")) {
+      int ledIndex = request->getParam("led")->value().toInt();
+      String state = request->getParam("state")->value();
+
+      if (ledIndex >= 1 && ledIndex <= LED_COUNT) {
+        digitalWrite(LED_PINS[ledIndex - 1], (state == "on") ? HIGH : LOW);
+        request->send(200, "text/plain", "OK");
+      } else {
+        request->send(400, "text/plain", "Invalid LED index");
+      }
+    } else {
+      request->send(400, "text/plain", "Missing parameters");
+    }
+  });
+
+  // End of ELegant OTA Setup
+}
+
+ 
+void setup() {
+
+  //setting GPIO to initial values
+  pinMode(WiFiLed, OUTPUT);         // set the LED pin mode
+  digitalWrite(WiFiLed, HIGH);      // turn the LED on (HIGH is the voltage level)
+  pinMode(LED_BUILTIN, OUTPUT);     // set the LED pin mode
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  pinMode(LED_1, OUTPUT);           // set the LED pin mode
+  digitalWrite(LED_1, HIGH);        // turn the LED on (HIGH is the voltage level)
+  pinMode(LED_2, OUTPUT);           // set the LED pin mode
+  digitalWrite(LED_2, HIGH);        // turn the LED on (HIGH is the voltage level)
+  pinMode(LED_3, OUTPUT);           // set the LED pin mode
+  digitalWrite(LED_3, HIGH);        // turn the LED on (HIGH is the voltage level)
+  pinMode(LED_4, OUTPUT);           // set the LED pin mode
+  digitalWrite(LED_4, HIGH);        // turn the LED on (HIGH is the voltage level)
+  pinMode(LED_5, OUTPUT);           // set the LED pin mode
+  digitalWrite(LED_5, HIGH);        // turn the LED on (HIGH is the voltage level)
+  Serial.begin(BAUDRATE);
+  while (!Serial) {
+    ;  // wait for serial port to connect. Needed for native USB
+  }
+  WiFiMan();
+  splashserial();
+  Wire.begin();
+  initOLED();
+  splashOLED();
+  initDFP();
+  splashDFPlayer();
+  setupButton();
+  initWiFi();
+  initLittleFS();
+  setupOTA();
+  server.begin();             // Start server
+  ElegantOTA.begin(&server);  // Start ElegantOTA
+  // More setup code here
+  digitalWrite(LED_1, LOW);        //Make built in LED low at end of setup.
+  digitalWrite(LED_2, LOW);        //Make built in LED low at end of setup.
+  digitalWrite(LED_3, LOW);        //Make built in LED low at end of setup.
+  digitalWrite(LED_4, LOW);        //Make built in LED low at end of setup.
+  digitalWrite(LED_5, LOW);        //Make built in LED low at end of setup.
+  digitalWrite(LED_BUILTIN, LOW);  //Make built in LED low at end of setup.
+} //end setup()
+
+void loop() {
+  wink();  // Heart beat aka activity indicator LED function.
+  led1.Update();
+  led2.Update();
+  led3.Update();
+  led4.Update();
+  led5.Update();
+  // This is needed to poll the button constantly
+  myButton.poll();
+  SW1.poll();
+  SW2.poll();
+  SW3.poll();
+  SW4.poll();
+}  //end loop()
