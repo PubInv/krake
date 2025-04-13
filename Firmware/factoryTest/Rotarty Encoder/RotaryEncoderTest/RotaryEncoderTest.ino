7fa8853c5adc05194d31db48a529fda8bc705d6b
Nagham <Naghamkheir25@gmail.com>
2025-04-13 11:32:06 -0400

    RotaryEncoderTest.ino, Adding Splash Serial to the Rotary Encoder Factory Test Firmware.

--- Firmware/factoryTest/Rotarty Encoder/RotaryEncoderTest/RotaryEncoderTest.ino
+++ Firmware/factoryTest/Rotarty Encoder/RotaryEncoderTest/RotaryEncoderTest.ino
@@ -1,7 +1,19 @@
+#define COMPANY_NAME "pubinv.org "
+#define PROG_NAME "RotaryEncoderTest"
+#define VERSION "V0.2 "
+#define DEVICE_UNDER_TEST "Krake: Rotary Encoder"  //A PMD model number
+#define LICENSE "GNU Affero General Public License, version 3 "
+#define ORIGIN "USA"
+
+#define BAUDRATE 115200
+
+
+
+
 // Define the GPIO pins for CLK, DT, and SW
-const int CLK = 39;   // Connected to the CLK pin of the encoder
-const int DT = 36;    // Connected to the DT pin of the encoder
-const int SW = 34;    // Connected to the SW (Switch) pin of the encoder
+const int CLK = 39;  // Connected to the CLK pin of the encoder
+const int DT = 36;   // Connected to the DT pin of the encoder
+const int SW = 34;   // Connected to the SW (Switch) pin of the encoder
 
 int currentStateCLK;
 int previousStateCLK;
@@ -10,15 +22,29 @@ bool isButtonPressed = false;
 
 void setup() {
   // Initialize serial communication for debugging
-  Serial.begin(115200);
+  // Start serial communication
+  Serial.begin(BAUDRATE);
+  while (!Serial) {
+    ;  // wait for serial port to connect. Needed for native USB
+  }
+  delay(500);
+  Serial.println("===================================");
+  Serial.println(DEVICE_UNDER_TEST);
+  Serial.print(PROG_NAME);
+  Serial.println(VERSION);
+  Serial.print("Compiled at: ");
+  Serial.println(F(__DATE__ " " __TIME__));  //compile date that is used for a unique identifier
+  Serial.println("===================================");
+  Serial.println();
 
-  // Set CLK and DT as inputs
-  pinMode(CLK, INPUT );
-  pinMode(DT, INPUT );
   
+  // Set CLK and DT as inputs
+  pinMode(CLK, INPUT);
+  pinMode(DT, INPUT);
+
   // Set SW (Switch) as input with internal pull-up resistor
   pinMode(SW, INPUT_PULLUP);
-  
+
   // Read the initial state of CLK
   previousStateCLK = digitalRead(CLK);
 }
@@ -30,7 +56,7 @@ void loop() {
   // If the current state of CLK is different from the last state
   // then the encoder has been rotated
   if (currentStateCLK != previousStateCLK) {
-    
+
     // If the DT state is different than the CLK state, the encoder
     // is rotating clockwise, otherwise it's counterclockwise
     if (digitalRead(DT) != currentStateCLK) {
@@ -62,5 +88,5 @@ void loop() {
   }
 
   // Small delay to debounce the switch and encoder
-//  delay(300);
+  //  delay(300);
 }
