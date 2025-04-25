def7d8ca42e8740ed03f9a4fae637e56b966f57f
Forrest Erickson <forresterickson@comcast.net>
2025-04-25 18:36:10 -0400

    DFPlayerTD5580ATest. Add OTA to sketch.

---
+++ Firmware/factoryTest/DFPlayerTD5580ATest/OTA.ino
@@ -0,0 +1,29 @@
+
+
+void setupOTA(){
+
+   WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Hi! I am ESP32.");
+  });
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+  ElegantOTA.begin(&server);    // Start ElegantOTA
+
+}//end setupOTA
