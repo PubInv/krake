e909d926a338178d48a594a4a04a657d722f0537
Nagham <Naghamkheir25@gmail.com>
2025-05-10 05:44:02 -0400

    PMD_BPM/ID.cpp, Enhanced the Serial Splash.

--- PMD/PMD_BPM/ID.cpp
+++ PMD/PMD_BPM/ID.cpp
@@ -1,17 +1,17 @@
 #include "ID.h"
 
 
-
 void splashserial() {
-    Serial.println(F("==================================="));
-    Serial.println(VERSION);
-    Serial.println(MODEL_NAME);
-    Serial.println(DEVICE_UNDER_TEST);
-    Serial.print(F("Compiled at: "));
-    Serial.println(F(__DATE__ " " __TIME__));
-    Serial.println(LICENSE);
-    Serial.println(F("==================================="));
-    Serial.println();
+ //Serial splash
+  Serial.println(F("==================================="));
+  Serial.print(PROG_NAME);
+  Serial.println(VERSION);
+  Serial.println(MODEL_NAME);
+  Serial.println(DEVICE_UNDER_TEST);
+  Serial.print(F("Compiled at: "));
+  Serial.println(F(__DATE__ " " __TIME__) ); //compile date that is used for a unique identifier
+  Serial.println(LICENSE);
+  Serial.println(F("==================================="));
+  Serial.println();
+
 }
-
-
