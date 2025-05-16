2ec411742f65e9bc3788da83c81ec582a3336c87
Nagham <Naghamkheir25@gmail.com>
2025-05-16 14:19:10 -0400

    MQTTmain.h, Added if condition to include MAC address to new KRAKE devices. Now on version V.0.1.4

--- PMD/PMD_BPM_wMQTT/MQTTmain.h
+++ PMD/PMD_BPM_wMQTT/MQTTmain.h
@@ -1,8 +1,8 @@
 #ifndef MQTTMAIN_H
 #define MQTTMAIN_H
 
-#include <WiFi.h>           // for WiFiClient
-#include <MQTT.h>           // for MQTTClient
+#include <WiFi.h>  // for WiFiClient
+#include <MQTT.h>  // for MQTTClient
 #include <WiFiClient.h>
 
 extern WiFiClient net;
@@ -11,16 +11,21 @@ extern MQTTClient client;
 #define BROKER "public.cloud.shiftr.io"
 #define CLIENT_NAME_MQTT "PubInv"
 
-// #define Nagham 
+#define Nagham
 #ifdef Nagham
-// LB0005 
+// LB0005
+#define SUBSCRIPTION_TOPIC "F024F9F1B880_ACK"
+#define PUBLISHING_TOPIC "F024F9F1B880_ALM"
+#endif
+// #define Robert
+#ifdef Robert
+// US0003
 #define SUBSCRIPTION_TOPIC "ECC9FF7C8C98_ACK"
-// #define PUBLISHING_TOPIC "F024F9F1B880_ALM"
-#define PUBLISHING_TOPIC "ECC9FF7C8C98_ALM" // publishing to Robert
-
-
-#else  // LEE
-// US0005 
+#define PUBLISHING_TOPIC "ECC9FF7C8C98_ALM"
+#endif
+// #define LEE
+#ifdef LEE  
+// US0005
 #define SUBSCRIPTION_TOPIC "ECC9FF7D8EE8_ACK"
 #define PUBLISHING_TOPIC "ECC9FF7D8EE8_ALM"
 #endif
