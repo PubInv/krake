bd114a1e0972358490ecd6bd6063991dd0de2dc1
Nagham <Naghamkheir25@gmail.com>
2025-05-28 15:00:54 -0400

    PMD_BPM_wMQTT, Version V.0.1.8, Configured a Last Will and Testament (LWT).

--- PMD/PMD_BPM_wMQTT/ID.h
+++ PMD/PMD_BPM_wMQTT/ID.h
@@ -2,7 +2,7 @@
 #include <Arduino.h>
 
 #define PROG_NAME "PMD_BPM_wMQTT "
-#define VERSION " V.0.1.7 " /// Adding wifi signal connected 
+#define VERSION " V.0.1.8 " /// Configure a Last Will and Testament (LWT) Message for an MQTT Client
 //#define MODEL_NAME "Model: BUTTOM"
 #define MODEL_NAME "Model: HW2_WiFiReady_Elegant"
 #define DEVICE_UNDER_TEST "SN: 00001"
--- PMD/PMD_BPM_wMQTT/MQTTmain.cpp
+++ PMD/PMD_BPM_wMQTT/MQTTmain.cpp
@@ -2,8 +2,9 @@
 #include <WiFi.h>
 #include <MQTT.h>
 #include <WiFiClient.h>
+#include "MQTTClient.h"
 
- 
+
 void connect() {
   Serial.print("checking wifi...");
   while (WiFi.status() != WL_CONNECTED) {
@@ -13,9 +14,20 @@ void connect() {
 
   Serial.print("\nconnecting...");
   while (!client.connect(CLIENT_NAME_MQTT, "public", "public")) {
+
+
+    // client.setWill(PUBLISHING_TOPIC, "a5 PMD device is disconnected from the broker. STALE DATA.", false, 2);
+
+    // client.setKeepAlive(60);
+
+
     Serial.print(".");
     delay(1000);
   }
+
+
+
+
 
   Serial.println("\nconnected!");
   client.subscribe(SUBSCRIPTION_TOPIC);
@@ -24,3 +36,26 @@ void connect() {
 void messageReceived(String& topic, String& payload) {
   Serial.println("incoming: " + topic + " - " + payload);
 }
+
+
+
+
+
+
+// const char payload[]= "LastWillMessage: a5 PMD device is disconnected from the broker. STALE DATA."
+// bool retained= false ;
+// int qos= 2 ;
+// int keepAlive = 60 ;
+// // void setWill(const char topic[], const char payload[], bool retained, int qos);
+// void setWill(const char topic[], const char payload[], bool retained, int qos);
+
+// void setKeepAlive(int keepAlive);
+
+
+
+
+//  setWill(PUBLISHING_TOPIC, "LastWillMessage: a5 PMD device is disconnected from the broker. STALE DATA.", false, 2);
+
+// CleanSession= true ;
+
+//  setKeepAlive(60);
