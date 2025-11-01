583c59d952b18761b5edc81d9ffcd2181e25d793
TheBakedPotato <11617336+TheBakedPotato@users.noreply.github.com>
2025-11-01 18:40:38 -0400

    271: Started laying out the 'loop back' test more

--- Firmware/factoryTestNew/lib/uart/LoopBack.h
+++ Firmware/factoryTestNew/lib/uart/LoopBack.h
@@ -9,11 +9,15 @@ public:
     LoopBack(int8_t uart1ReceivePin, int8_t uart1TransmitPin, int8_t uart2ReceivePin, int8_t uart2TransmitPin, unsigned long baudRate = DEFAULT_BAUD_RATE);
     ~LoopBack();
 
+    void run(HardwareSerial &serial);
+
 private:
     static const unsigned long DEFAULT_BAUD_RATE = 115200;
 
     HardwareSerial uart1;
     HardwareSerial uart2;
+
+    int counter;
 };
 
 #endif
--- Firmware/factoryTestNew/src/main.ino
+++ Firmware/factoryTestNew/src/main.ino
@@ -34,60 +34,21 @@ Modified to add UART 2 on pins GPIO 16 and 17 by Forrest Lee Erickson, Dec. 16, 
 
 #include <Arduino.h>
 
-// Use for UART communication
-HardwareSerial mySerialUART1(1);
-HardwareSerial mySerialUART2(2);
+#include <LoopBack.h>
 
-int counter = 0;
+LoopBack loopBackTest(RXD1, TXD1, RXD2, TXD2);
 
 void setup()
 {
     Serial.begin(115200); // UART 0 setup
-    while (!Serial)
-    {
-        ; // wait for serial port to connect. Needed for native USB
-    }
-
-    mySerialUART1.begin(115200, SERIAL_8N1, RXD1, TXD1); // UART1 setup
-    while (!mySerialUART1)
-    {
-        ; // wait for UART1 to connect.
-    }
-    mySerialUART2.begin(115200, SERIAL_8N1, RXD2, TXD2); // UART2 setup
-    while (!mySerialUART2)
-    {
-        ; // wait for UART2 to connect.
-    }
 
     delay(500);
     Serial.println("ESP32 UART0 Transmitter");
-    mySerialUART1.println("ESP32 UART1 Transmitter");
-    mySerialUART2.println("ESP32 UART2 Transmitter");
+    // mySerialUART1.println("ESP32 UART1 Transmitter");
+    // mySerialUART2.println("ESP32 UART2 Transmitter");
 }
 
 void loop()
 {
-
-    while (mySerialUART1.available())
-    {
-        Serial.println("Bytes Available UART1! :");
-        Serial.write(mySerialUART1.read());
-    }
-
-    while (mySerialUART2.available())
-    {
-        //   Serial.println("Bytes Available! :");
-        Serial.write(mySerialUART2.read());
-    }
-
-    // Send message over UART
-    mySerialUART1.print("UART1: ");
-    mySerialUART1.println(String(counter % 6));
-    // mySerialUART1.println("\n");
-
-    Serial.println("Sent: " + String(counter % 6));
-
-    counter++; // increment the counter so the message changes.
-
-    delay(1000); // Lets not got too fast to read.
+    loopBackTest.run(Serial);
 }
