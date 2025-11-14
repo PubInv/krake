4c37ced383fdb8e62eb4c95d08afa4bf9111b1f6
TheBakedPotato <11617336+TheBakedPotato@users.noreply.github.com>
2025-11-13 22:09:45 -0500

    Feature: #206 - Defined GPAD_API_VERSION value and publish value
    - GPAD_API_VERSION is now defined as a preprocessor directive, like
      FIRMWARE_VERSION
    - The value is published via MQTT just like the FIRMWARE_VERSION

--- Firmware/GPAD_API/pre_extra_script.py
+++ Firmware/GPAD_API/pre_extra_script.py
@@ -3,7 +3,8 @@ Import("env")
 cpp_defines = [
     ("COMPANY_NAME", "PubInv "),   # For the Broker ID for MQTT 
     ("PROG_NAME", "GPAD_API "),    # This program
-    ("FIRMWARE_VERSION", "0.46 "), # Initial Menu implementation
+    ("FIRMWARE_VERSION", "0.45 "), # Initial Menu implementation
+    ("GPAD_API_VERSION", "1.0.0"),
     ("MODEL_NAME", "KRAKE_"), 
     ("LICENSE", "GNU Affero General Public License, version 3 "),
     ("ORIGIN", "US"),
