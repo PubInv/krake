830df77f4e483667eb4ffcbc5826012a2b70bf43
TheBakedPotato <11617336+TheBakedPotato@users.noreply.github.com>
2025-11-16 15:03:46 -0500

    (#273): Auto formatting/styling

--- Firmware/GPAD_API/src/InterruptRotator.cpp
+++ Firmware/GPAD_API/src/InterruptRotator.cpp
@@ -1,16 +1,17 @@
 #include "InterruptRotator.h"
 #include "GPAD_menu.h"
 
-static RotaryEncoder* encoder = nullptr;
+static RotaryEncoder *encoder = nullptr;
 
 // This global variable represents the state of the menu;
-// we are either running the menu (true) or displaying other 
+// we are either running the menu (true) or displaying other
 // information (false)
 extern bool running_menu;
 
-void initRotator() {
-  //  Serial.begin(115200);
-   // while (!Serial);
+void initRotator()
+{
+    //  Serial.begin(115200);
+    // while (!Serial);
     Serial.println("InterruptRotator example for the RotaryEncoder library.");
 
     encoder = new RotaryEncoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
@@ -19,29 +20,32 @@ void initRotator() {
     attachInterrupt(digitalPinToInterrupt(PIN_IN2), checkPositionISR, CHANGE);
 }
 
-void updateRotator() {
+void updateRotator()
+{
     static int pos = 0;
 
     encoder->tick();
     int newPos = encoder->getPosition();
 
-    if (pos != newPos) {
-
+    if (pos != newPos)
+    {
 
         Serial.print("pos: ");
         Serial.print(newPos);
         Serial.print(" dir: ");
         // If we have rotated the encoder, then we enter the menu...
         if (!running_menu)
-            reset_menu_navigation();        
+            reset_menu_navigation();
 
         int d = (int)(encoder->getDirection());
-  //      Serial.println(d);
+        //      Serial.println(d);
 
-   //     int d = (int)(encoder->getDirection());
+        //     int d = (int)(encoder->getDirection());
         bool CW;
-        if (d == (int) RotaryEncoder::Direction::CLOCKWISE) CW = true;
-        else CW = false;
+        if (d == (int)RotaryEncoder::Direction::CLOCKWISE)
+            CW = true;
+        else
+            CW = false;
         // Serial.print("d : ");
         // Serial.println(d);
         // Serial.println((int) RotaryEncoder::Direction::CLOCKWISE);
@@ -52,8 +56,10 @@ void updateRotator() {
     }
 }
 
-void IRAM_ATTR checkPositionISR() {
-    if (encoder != nullptr) {
+void IRAM_ATTR checkPositionISR()
+{
+    if (encoder != nullptr)
+    {
         encoder->tick();
     }
 }
--- Firmware/GPAD_API/src/gpad_utility.h
+++ Firmware/GPAD_API/src/gpad_utility.h
@@ -22,25 +22,25 @@
 #define GPAD_UTILITY 1
 #include <Stream.h>
 #ifndef COMPANY_NAME
-  #define COMPANY_NAME ""
+#define COMPANY_NAME ""
 #endif
 #ifndef PROG_NAME
-  #define PROG_NAME ""
+#define PROG_NAME ""
 #endif
 #ifndef FIRMWARE_VERSION
-  #define FIRMWARE_VERSION ""
+#define FIRMWARE_VERSION ""
 #endif
-//#define HARDWARE_VERSION "V0.0.1 "
+// #define HARDWARE_VERSION "V0.0.1 "
 #ifndef MODEL_NAME
-  #define MODEL_NAME ""
+#define MODEL_NAME ""
 #endif
 #ifndef LICENSE
-  #define LICENSE "GNU Affero General Public License, version 3 "
+#define LICENSE "GNU Affero General Public License, version 3 "
 #endif
 #ifndef ORIGIN
-  #define ORIGIN ""
+#define ORIGIN ""
 #endif
-#define DEVICE_UNDER_TEST "Krake: DFPlayer"  //This is GPAD code, but if it is used in testing...
+#define DEVICE_UNDER_TEST "Krake: DFPlayer" // This is GPAD code, but if it is used in testing...
 
 // THIS IS FOR DEBUGGING
 #define LIMIT_POWER_DRAW 1
