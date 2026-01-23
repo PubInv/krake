5728f70c0b1f95ff4f09a956dddcdc8c015b5c69
TheBakedPotato <11617336+TheBakedPotato@users.noreply.github.com>
2026-01-22 20:01:53 -0500

    (#333): Pulls the latest committed tag to insert into the compiler build flag for FIRWARE_VERSION

--- Firmware/GPAD_API/pre_extra_script.py
+++ Firmware/GPAD_API/pre_extra_script.py
@@ -1,9 +1,20 @@
 Import("env")
 
+try:
+    import git
+except ImportError:
+    env.Execute("$PYTHONEXE -m pip install GitPython")
+
+repo = git.Repo('../../')
+tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
+latest_tag = "unknown"
+if len(tags) > 0:
+    latest_tag = [-1]
+
 cpp_defines = [
     ("COMPANY_NAME", "PubInv "),   # For the Broker ID for MQTT 
     ("PROG_NAME", "GPAD_API "),    # This program
-    ("FIRMWARE_VERSION", "0.46 "), # Initial Menu implementation
+    ("FIRMWARE_VERSION", f"{latest_tag} "),
     ("MODEL_NAME", "KRAKE_"), 
     ("LICENSE", "GNU Affero General Public License, version 3 "),
     ("ORIGIN", "US"),
