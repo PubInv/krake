529ea82c24ec2c3a96abe45da6a0083c1a01ce9c
TheBakedPotato <11617336+TheBakedPotato@users.noreply.github.com>
2026-03-03 21:22:36 -0500

    (#387): Added more constructors/deserialization methods

--- Firmware/GPAD_API/GPAD_API/protocol.cpp
+++ Firmware/GPAD_API/GPAD_API/protocol.cpp
@@ -2,25 +2,45 @@
 
 using namespace protocol;
 
-const AlarmLevel DEFAULT_ALARM_LEVEL = AlarmLevel::Level1;
+AlarmCommand::AlarmCommand(AlarmLevel alarmLevel, const char *const alarmMessage) : level(alarmLevel), alarm(alarmMessage) {}
 
-AlarmCommand::AlarmCommand() : level(DEFAULT_ALARM_LEVEL)
-{
-}
-
-AlarmCommand::AlarmCommand(AlarmLevel level) : level(level) {}
-
-AlarmCommand AlarmCommand::deserialize()
+AlarmCommand AlarmCommand::deserialize(const char *const messageBytes)
 {
     AlarmLevel level = AlarmLevel::Level1;
-    return AlarmCommand(level);
+    return AlarmCommand(level, messageBytes);
 }
 
 InfoCommand::InfoCommand()
 {
 }
 
-InfoCommand InfoCommand::deserialize()
+InfoCommand InfoCommand::deserialize(const char *const messageBytes)
 {
     return InfoCommand();
+}
+
+Command::Command(const InfoCommand infoCommand) : info(infoCommand) {}
+Command::Command(const AlarmCommand alarmCommand) : alarm(alarmCommand) {}
+
+ProtocolMessage::ProtocolMessage(CommandType commandType, Command command) : commandType(commandType), command(command) {}
+
+ProtocolMessage ProtocolMessage::deserialize(const char *const messageBytes)
+{
+    CommandType commandType = static_cast<CommandType>(messageBytes[0]);
+
+    const char *const remainingBytes = messageBytes + 1;
+    Command command = [commandType, remainingBytes]()
+    {
+        switch (commandType)
+        {
+        case CommandType::ALARM:
+            return Command(AlarmCommand::deserialize(remainingBytes));
+        case CommandType::INFO:
+            return Command(InfoCommand::deserialize(remainingBytes));
+        }
+    }();
+
+    Command command = Command(InfoCommand());
+
+    return ProtocolMessage(commandType, command);
 }
