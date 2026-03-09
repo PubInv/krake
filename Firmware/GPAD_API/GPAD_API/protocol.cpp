87e975dc34c1ce485fb30959c700818a395cd7c0
TheBakedPotato <11617336+TheBakedPotato@users.noreply.github.com>
2026-03-08 21:31:54 -0400

    (#387): Constructors/operators support const types as well now

--- Firmware/GPAD_API/GPAD_API/protocol.cpp
+++ Firmware/GPAD_API/GPAD_API/protocol.cpp
@@ -2,27 +2,38 @@
 
 using namespace protocol;
 
-AlarmCommand::AlarmCommand(const AlarmCommand::Level alarmLevel, const std::array<char, 80> alarmMessage) : level(alarmLevel), message(alarmMessage) {}
-
-AlarmCommand AlarmCommand::deserialize(const char *const messageBytes)
+AlarmMessage::AlarmMessage(const char *const messageBytes, const size_t numBytes)
 {
-    // TODO: Throw error if there is no 0th element;
+    auto index = 0;
+    for (; index < numBytes; ++index)
+    {
+        this->messageBytes.at(index) = messageBytes[index];
+    }
+}
+
+AlarmCommand::AlarmCommand(const AlarmCommand::Level alarmLevel, const AlarmMessage alarmMessage) : level(alarmLevel), message(std::move(alarmMessage)) {}
+
+AlarmCommand AlarmCommand::deserialize(const char *const messageBytes, const size_t numBytes)
+{
+    // TODO: This will throw if either there is no 0th element or the static cast fails
+    // Those should be differentiated into 2 exceptions for traceability
     const auto level = static_cast<AlarmCommand::Level>(messageBytes[0]);
 
-    const auto level = AlarmCommand::Level::Level1;
-    return AlarmCommand(level, std::array<char, 80>());
+    const auto alarmMessage = AlarmMessage(messageBytes + 1, numBytes - 1);
+
+    return AlarmCommand(level, std::move(alarmMessage));
 }
 
 InfoCommand::InfoCommand()
 {
 }
 
-InfoCommand InfoCommand::deserialize(const char *const messageBytes)
+InfoCommand InfoCommand::deserialize(const char *const messageBytes, const size_t numBytes)
 {
     return InfoCommand();
 }
 
-const ProtocolMessage ProtocolMessage::deserialize(const char *const messageBytes)
+const ProtocolMessage ProtocolMessage::deserialize(const char *const messageBytes, const size_t numBytes)
 {
     // TODO: This should be wrapped in a try/catch if there isn't a 0th element
     auto commandType = static_cast<CommandType>(messageBytes[0]);
@@ -32,8 +43,9 @@ const ProtocolMessage ProtocolMessage::deserialize(const char *const messageByte
     switch (commandType)
     {
     case CommandType::ALARM:
-        return ProtocolMessage(AlarmCommand::deserialize(remainingBytes));
+        return ProtocolMessage(AlarmCommand::deserialize(remainingBytes, numBytes));
     case CommandType::INFO:
-        return ProtocolMessage(InfoCommand::deserialize(remainingBytes));
+        return ProtocolMessage(InfoCommand::deserialize(remainingBytes, numBytes));
     }
+    return ProtocolMessage();
 }
