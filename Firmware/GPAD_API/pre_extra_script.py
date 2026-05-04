c43b12c8de799f46d075b46a6fc18723eced2d2a
nk25719 <naghamkheir25@gmail.com>
2026-05-04 19:54:47 +0300

    incrementing LittleFS to V0.1.6

--- Firmware/GPAD_API/pre_extra_script.py
+++ Firmware/GPAD_API/pre_extra_script.py
@@ -4,7 +4,7 @@ cpp_defines = [
     ("COMPANY_NAME", "PubInv "),   # For the Broker ID for MQTT 
     ("PROG_NAME", "GPAD_API "),    # This program
     ("FIRMWARE_VERSION", "0.55 "), # pr 501
-    ("LittleFS_VERSION", "0.1.5 "), # pr 501
+    ("LittleFS_VERSION", "0.1.6 "), # pr 501
     ("MODEL_NAME", "KRAKE_"), 
     ("LICENSE", "GNU Affero General Public License, version 3 "),
     ("ORIGIN", "US"),
